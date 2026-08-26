3297a26493a0e5363f19d3adef22ad6005e367ef
CHgopi07 <2410040007@klh.edu.in>
2026-08-26 23:59:20 +0530

    Add solution for Two Sum

--- LeetCode/Two-Sum/Solution.java
+++ LeetCode/Two-Sum/Solution.java
@@ -1,17 +1,12 @@
-1class Solution {
-2    public int[] twoSum(int[] nums, int target) {
-3        java.util.HashMap<Integer, Integer> map = new java.util.HashMap<>();
-4
-5        for (int i = 0; i < nums.length; i++) {
-6            int need = target - nums[i];
-7
-8            if (map.containsKey(need)) {
-9                return new int[]{map.get(need), i};
-10            }
-11
-12            map.put(nums[i], i);
-13        }
-14
-15        return new int[]{};
-16    }
-17}
+ for(var i =0;i<nums.length;i++){
+    var item = nums[i];   
+    for(var j=0; j < nums.length;j++){
+        var item2 = nums[j];
+        var sum = item + item2;
+        
+    if(sum === target ){
+      return [i,j];
+      
+    }
+  }
+ }
